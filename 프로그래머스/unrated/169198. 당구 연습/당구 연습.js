0f4d2751925ae93c3ec8aaac7a3295d06caa9059
KangBit <kang.b.c.1208@gmail.com>
2023-03-20 14:52:29 +0900

    [unrated] Title: 당구 연습, Time: 2.86 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/169198. 당구 연습/당구 연습.js
@@ -0,0 +1,38 @@
+function solution(m, n, startX, startY, balls) {
+    return balls.reduce((acc, curr) => {
+        let dist = getNearlist(m, n, startX, startY, curr[0], curr[1]);
+        
+        return [...acc, Math.min(dist)];
+    }, []);
+}
+
+const getNearlist = (m, n, x1, y1, x2, y2) => {
+    let [left, right, top, bottom] = [
+        [-x1, y1],
+        [m + m - x1, y1],
+        [x1, n + n - y1],
+        [x1, -y1]
+    ];
+    
+    if (x1 === x2 && y1 > y2) {
+        bottom = undefined;
+    }
+    if (x1 === x2 && y1 < y2) {
+        top = undefined;
+    }
+    if (y1 === y2 && x1 > x2) {
+        left = undefined;
+    }
+    if (y1 === y2 && x1 < x2) {
+        right = undefined;
+    }
+    
+    return [left, right, top, bottom].reduce((min, curr) => {
+        if (curr === undefined) {
+            return min;
+        }
+        
+        const [x, y] = curr;
+        return Math.min(min, (x-x2)**2 + (y-y2)**2);
+    }, m**2 + n**2);
+}
