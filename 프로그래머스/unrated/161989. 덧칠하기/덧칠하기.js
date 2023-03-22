36ed472dc8fba24b93af8050feecdb0697a5a588
KangBit <kang.b.c.1208@gmail.com>
2023-03-22 13:33:49 +0900

    [unrated] Title: 덧칠하기, Time: 66.43 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,10 @@
+function solution(n, m, section) {
+    let answer = 0;
+    for(let i = 1; i <= n; i++) {
+        if (section.includes(i)){
+            i += m-1
+            answer += 1
+        }
+    }
+    return answer;
+}
