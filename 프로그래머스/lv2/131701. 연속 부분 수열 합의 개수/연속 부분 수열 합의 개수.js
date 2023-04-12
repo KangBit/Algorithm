fe0226a8145b268eddd260f710f48ef6750de95a
KangBit <kang.b.c.1208@gmail.com>
2023-04-12 11:10:06 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1960.48 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,30 @@
+// function solution(elements) {
+//     let answer = new Set();
+    
+//     for(let i = 1; i < elements.length ; i++) {
+//         elements.forEach((el, l) => {
+//             let sum = 0;
+//             for(let j = 0; j < i; j++) {
+//                 sum = sum + elements[(l + j) % elements.length];
+//             }
+//             answer.add(sum);
+//         });
+//     }
+//     return answer.size + 1;
+// }
+
+
 function solution(elements) {
-    let answer = new Set();
+    let answer = [];
+    
     for(let i = 1; i < elements.length ; i++) {
         elements.forEach((el, l) => {
             let sum = 0;
             for(let j = 0; j < i; j++) {
                 sum = sum + elements[(l + j) % elements.length];
             }
-            answer.add(sum);
+            answer.push(sum);
         });
     }
-    return answer.size + 1;
+    return new Set(answer).size + 1;
 }
