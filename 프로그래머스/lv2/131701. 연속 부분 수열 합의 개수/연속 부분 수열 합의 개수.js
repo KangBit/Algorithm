3979c4d6a901571ae31d69d1396c39f696db42f3
KangBit <kang.b.c.1208@gmail.com>
2023-04-12 11:28:17 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 106.44 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,15 +1,13 @@
 function solution(elements) {
-    let arr = [];
+    const size = elements.length;
+    const arr = elements.reduce((acc, curr, i)=>{
+        let sum = curr;
+        for (let l = 1; l < size - 1; l++) {
+            sum = sum + elements[(i + l) % size];
+            acc.push(sum);
+        }
+        return acc;
+    }, elements.slice());
     
-    for(let size = 1; size < elements.length ; size++) {
-        elements.forEach((el, i) => {
-            let sum = 0;
-            for(let j = 0; j < size; j++) {
-                sum = sum + elements[(i + j) % elements.length];
-            }
-            arr.push(sum);
-        });
-    }
     return new Set(arr).size + 1;
 }
-
