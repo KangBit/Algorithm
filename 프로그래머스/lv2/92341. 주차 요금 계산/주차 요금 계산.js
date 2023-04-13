59e625dca30bee5c874625cf1224427aa7e602dd
KangBit <kang.b.c.1208@gmail.com>
2023-04-13 13:57:33 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,57 @@
+function solution(fees, records) {
+    let parkingSpace = new Map();
+    let cars = new Map();
+    
+    records.forEach((record,idx)=>{
+        const [time, number, inout] = record.split(" ");
+        let parkingTime = 0;
+        
+        if(parkingSpace.has(number)) {
+            parkingTime = parkingTime + getParkingTime(parkingSpace.get(number), time);
+            parkingSpace.delete(number);
+        }else {
+            parkingSpace.set(number, time);
+        }
+        
+        if(cars.has(number)){
+            parkingTime = parkingTime + cars.get(number);
+        }
+
+        cars.set(number, parkingTime);
+    });
+    
+    parkingSpace.forEach((time, number) => {
+        let parkingTime = getParkingTime(time);
+        if(cars.has(number)){
+            parkingTime = parkingTime + cars.get(number);
+        }
+
+        cars.set(number, parkingTime);
+    })
+    
+    return Array.from(cars)
+        .sort((a, b) => +a[0] - +b[0])
+        .map(([carNumber, parkingTime]) => {
+            return getParkingFee(fees, parkingTime);
+        });
+}
+
+const getParkingTime = (inTime, outTime = "23:59") => {
+    let [outH, outM] = outTime.split(":");
+    let [inH, inM] = inTime.split(":");
+    
+    return (+outM - +inM) + (+outH - +inH) * 60;
+}
+
+const getParkingFee = (fees, time) => {
+    const [basicTime, basicFee, unitTime, unitFee] = fees;
+    let fee = basicFee;
+    
+    if(time > basicTime) {
+        const overTime = time - basicTime;
+        const overTimeFee = Math.ceil(overTime/unitTime) * unitFee;
+        fee = fee + overTimeFee;
+    }
+    
+    return fee;
+}
