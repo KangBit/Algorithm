bff11fb59557eedb7888b60a2fafb5c225fc4dc9
KangBit <kang.b.c.1208@gmail.com>
2023-06-07 13:23:37 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.46 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,33 @@
+function solution(x, y, n) {
+    let answer = 0;
+    let flag = true;
+    let set = new Set([x]);
+
+    while(flag) {
+        if(set.size === 0) {
+            return -1;
+        }
+        
+        if(set.has(y)) {
+            return answer
+        }
+        
+        set = [...set].reduce((acc, curr)=>{
+            if(curr + n <= y) {
+                acc.add(curr + n);
+            }
+            if(curr * 2 <= y) {
+                acc.add(curr * 2);
+            }
+            if(curr * 3 <= y) {
+                acc.add(curr * 3);
+            }
+
+            acc.delete(curr);
+
+            return acc;
+        },new Set());
+        
+        answer = answer + 1;
+    }
+}
