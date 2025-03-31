d7a838017a0b8485e4ee2547608b402a6f77aa7c
KangBit <kang.b.c.1208@gmail.com>
2025-03-31 16:26:43 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -0,0 +1,16 @@
+function solution(n, w, num) {
+    const floor = Math.ceil(num % w);
+    const t = num % w || w;
+
+    let answer = 0;
+    let gap = (2 * w) - (2 * t) + 1;
+    let climb = num;
+    
+    while(climb <= n) {
+        answer = answer + 1;
+        climb = climb + gap;
+        gap = 2 * w - gap;
+    }
+    
+    return answer;
+}
