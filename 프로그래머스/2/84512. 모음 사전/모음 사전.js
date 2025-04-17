7629275c6650cd36e4aea0cc6ea45cbbd3a1a5ca
KangBit <kang.b.c.1208@gmail.com>
2025-04-17 16:17:43 +0900

    [level 2] Title: 모음 사전, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,19 +1,37 @@
-const chars = [" ", "A", "E", "I", "O", "U"];
+// 1. 재귀를 이용해 사전 만들기
+// const chars = [" ", "A", "E", "I", "O", "U"];
 
+// function solution(word) {
+//   const dict = [...makeWords("")].sort();
+//   return dict.indexOf(word);
+// }
+
+// const makeWords = (str) => {
+//   const trimmedStr = str.trim();
+//   if (str.length === 5 || trimmedStr.length !== str.length) {
+//     return new Set([trimmedStr]);
+//   }
+
+//   return chars.reduce((acc, char) => {
+//     return union(acc, makeWords(str + char));
+//   }, new Set());
+// };
+
+// const union = (setA, setB) => new Set([...setA, ...setB]);
+
+// 2. 가중치 이용하기
 function solution(word) {
-  const dict = [...makeWords("")].sort();
-  return dict.indexOf(word);
+    const chars = ["A", "E", "I", "O", "U"];
+    
+    const weights = Array.from({length: 5}).map((_, i) => {
+        return (5**(5-i)-1) / 4;
+    })
+    
+    return word.split('').reduce((acc, char, i) => {
+        return acc + 1 + chars.indexOf(char)* weights[i];
+    }, 0)
 }
 
-const makeWords = (str) => {
-  const trimmedStr = str.trim();
-  if (str.length === 5 || trimmedStr.length !== str.length) {
-    return new Set([trimmedStr]);
-  }
-
-  return chars.reduce((acc, char) => {
-    return union(acc, makeWords(str + char));
-  }, new Set());
-};
-
-const union = (setA, setB) => new Set([...setA, ...setB]);
+// 3. dfs 이용하기
+// function solution(word) {
+// }
